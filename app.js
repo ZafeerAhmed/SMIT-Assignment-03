e782eddb55f0bd986b31329a3a34e0e9febf0f2d
Zafeer Ahmed <zafe2904@gmail.com>
2022-08-27 10:52:13 -0700

    2nd Question
    Check whether a passed string is palindrone or not?

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-// 1. Write a JavaScript function that reverse a number.
-// Example x = 32243;
-// Expected Output : 34223
+// 2. Write a JavaScript function that checks whether a passed string is palindrome or not?
+// A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam, radar or level.
 
-const num = 124323;
-const reverse = (num) => parseInt(String(num)
-.split("")
-.reverse()
-.join(""), 10);
-console.log(reverse(num));
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
+
 
 
